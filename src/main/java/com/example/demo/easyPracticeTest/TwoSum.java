b602a46c66f59983d05f2b3aab2c6224214dc402
pragyakumar2103 <pragya.kumar2103@gmail.com>
2024-08-02 21:47:24 +0530

    twoSum description

--- src/main/java/com/example/demo/easyPracticeTest/TwoSum.java
+++ src/main/java/com/example/demo/easyPracticeTest/TwoSum.java
@@ -29,3 +29,17 @@ public class TwoSum {
         return new int[0]; // Return an empty array if no solution is found
     }
 }
+/*
+twoSumA :
+Intuition
+Using Hashmap to keep track of elements and their indices.getting the compliment for each element and checking if it exists in the hashmap. otherwise storing it n=in the hashmap with its index.
+
+Approach
+Initializing Hashmap is a constant time operation[O(1)]-time.
+Iterate through the array once, computing its compliment[O(n)]-time.
+worst case scenario, we need to store until the last elements[O(n)]-space.
+
+Complexity
+Time complexity:O(n)    2ms
+Space complexity:O(n)   45.16MB
+*/
