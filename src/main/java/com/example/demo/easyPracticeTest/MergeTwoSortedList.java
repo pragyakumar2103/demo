af9029c7d9968cfdd21371f33a6c93796a280455
pragyakumar2103 <pragya.kumar2103@gmail.com>
2024-08-03 10:35:05 +0530

    MergedTwoSortedList

--- src/main/java/com/example/demo/easyPracticeTest/MergeTwoSortedList.java
+++ src/main/java/com/example/demo/easyPracticeTest/MergeTwoSortedList.java
@@ -10,8 +10,24 @@ public class MergeTwoSortedList {
    //todo: https://leetcode.com/problems/merge-two-sorted-lists/description/
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode list = new ListNode();
+        ListNode current = list;
+        while(list1!=null && list2!=null){
+            if (list1.val>=list2.val){
+                current.next = list2;
+                list2 = list2.next;
+            } else {
+                current.next = list1;
+                list1 = list1.next;
+            }
+            current = current.next;
+        }
+        if (list1!=null)
+            current.next = list1;
+        else
+            current.next = list2;
 
-        return list1;
+        return list.next;
     }
     public class ListNode{
         int val;
@@ -21,3 +37,21 @@ public class MergeTwoSortedList {
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 }
+/*
+* Intuition
+* list node that serves as a placeholder to build the merged list
+* current pointer to keep track of the last node in the merged list
+* initially current is set to the list node.
+*
+* Approach
+* Iterate through each both input list, when both are not null, compare the values,
+* append the smaller value to the merged list by setting current.next to the node with smaller value
+* and then move the pointer to the next node in the merged list.
+* appending the remaining node of the non-exhausted list to the merged list
+* return the list.next as the head of the merged list(while skipping the list node itself)
+*
+* Complexity
+Time complexity:O(n+m)    0ms
+Space complexity:O(1)   42.52MB (excluding the space for the output list, we are only using few additional pointers)
+* n - length of list1, m -  length of list2
+* */
